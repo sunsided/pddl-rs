1872666e038d58d8eb5cbddb21e40762e42e08a7
Markus Mayer <widemeadows@gmail.com>
2023-04-16 22:21:39 +0200

    Simplify empty_or parser combinator
    This replaces map_res with map for a simpler logic.

--- src/parsers/empty_or.rs
+++ src/parsers/empty_or.rs
@@ -1,7 +1,6 @@
-use std::convert::Infallible;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::combinator::map_res;
+use nom::combinator::map;
 use nom::IResult;
 
 /// Parser combinator that takes a parser `inner` and produces a parser that
@@ -17,20 +16,19 @@ use nom::IResult;
 /// ```
 #[allow(dead_code)]
 pub fn empty_or<'a, F, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, Option<O>>
-    where
-        F: FnMut(&'a str) -> IResult<&'a str, O>,
+where
+    F: FnMut(&'a str) -> IResult<&'a str, O>,
 {
-    let empty_parser = map_res(tag("()"), |_: &str| Ok::<Option<O>, Infallible>(None));
-    let inner_parser = map_res(inner, |o: O| Ok::<Option<O>, Infallible>(Some(o)));
+    let empty_parser = map(tag("()"), |_: &str| None);
+    let inner_parser = map(inner, |o: O| Some(o));
 
     alt((empty_parser, inner_parser))
 }
 
-
 #[cfg(test)]
 mod tests {
-    use nom::character::complete::alpha1;
     use super::*;
+    use nom::character::complete::alpha1;
 
     #[test]
     fn empty_or_works() {
