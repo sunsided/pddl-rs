6b3026cde3a0672e215acb910c6123a1a0f21ac7
Markus Mayer <widemeadows@gmail.com>
2023-04-23 19:47:44 +0200

    Add automatic implementation of visitors

--- src/visitor.rs
+++ src/visitor.rs
@@ -18,6 +18,28 @@ pub trait AcceptMut<V: VisitorMut<T, O>, T, O> {
     fn accept_mut(&self, v: &mut V) -> O;
 }
 
+/// Automatic implementation for accepting immutable visitors
+/// if the visitor recognizes the target type.
+impl<V, T, O> Accept<V, T, O> for T
+where
+    V: Visitor<T, O>,
+{
+    fn accept(&self, v: &V) -> O {
+        v.visit(self)
+    }
+}
+
+/// Automatic implementation for accepting mutable visitors
+/// if the visitor recognizes the target type.
+impl<V, T, O> AcceptMut<V, T, O> for T
+where
+    V: VisitorMut<T, O>,
+{
+    fn accept_mut(&self, v: &mut V) -> O {
+        v.visit_mut(self)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,12 +50,6 @@ mod tests {
     impl VisitorMut<Item, ()> for Visi {
         fn visit_mut(&mut self, value: &Item) -> () {
             self.0 += value.0 as isize;
-        }
-    }
-
-    impl AcceptMut<Visi, Item, ()> for Item {
-        fn accept_mut(&self, v: &mut Visi) -> () {
-            v.visit_mut(self)
         }
     }
 
